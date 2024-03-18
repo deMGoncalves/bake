ac47f14676ae4cc5ef07551f95eaa9f95aa736f2
deMGoncalves <cleber.engineer@hotmail.com>
2024-03-18 12:30:32 -0300

    refactor: troca o table por log

--- src/bake.ts
+++ src/bake.ts
@@ -5,7 +5,7 @@ class Bake {
   static async run (path: string): Promise<void> {
     const sourceCode: SourceCode = await SourceCode.from(path)
     const tokens: Array<Token> = Scanner.run(sourceCode)
-    console.table(tokens)
+    console.log(tokens)
   }
 }
 
