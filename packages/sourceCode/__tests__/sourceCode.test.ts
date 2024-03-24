a7b54c839d872e27d37b6d3fa2cba64f0d73b73d
deMGoncalves / <cleber.engineer@hotmail.com>
2024-03-23 23:04:55 -0300

    Update sourceCode.test.ts

--- packages/sourceCode/__tests__/sourceCode.test.ts
+++ packages/sourceCode/__tests__/sourceCode.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, test } from 'bun:test'
 
 describe('sourceCode', () => {
-  test.todo('');
+  test.todo('Metodo peek retorna o caracter atual')
+  test.todo('Metodo lookAhead retona um range de caracteres a frente do caracter atual')
+  test.todo('Metodo shift retorna o lexema do caracter atual')
+  test.todo('Metodo take retorna o lexema de um range de caracteres')
 })
